88b84a49ab31fec13f57231be49f95e461b54a0f
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2025-11-11 20:46:16 +0100

    Run with big test case

--- support/stack.h
+++ support/stack.h
@@ -9,49 +9,23 @@
 
 USE ABOR1_ACC_MOD
 USE PARKIND1, ONLY : JPRB
-USE ISO_C_BINDING, ONLY : C_PTR, C_F_POINTER
-
-#ifdef USE_STACK
+USE ISO_C_BINDING, ONLY : C_LOC, C_F_POINTER
 
 #define temp(t, n, s) t, POINTER, CONTIGUOUS, DIMENSION s :: n
-
-#define alloc(n) IP_##n##_=YLSTACK%L;YLSTACK%L=YLSTACK%L+MAX(JPRB,KIND(n))*SIZE(n);IF(YLSTACK%L>YLSTACK%U)CALL ABOR1_ACC(__FILE__)
 
 #define assoc(p,q) IP_##p##_ = LOC(q)
 
 #define nullptr(p) IP_##p##_ = 0
 
-#define alloc4(n, s, l) CALL C_F_POINTER(TRANSFER(YLSTACK%L4, CPTR), n, SHAPE=s); n l => n; YLSTACK%L4=YLSTACK%L4+4*SIZE(n);IF(YLSTACK%L4>YLSTACK%U4)CALL ABOR1_ACC(__FILE__)
+#define alloc4(n, s, l) CALL C_F_POINTER(C_LOC(YSTACK%ZDATA4(1,YLSTACK%L4+1)), n, SHAPE=s); n l => n; YLSTACK%L4=YLSTACK%L4+(SIZE(n,KIND=8)/SIZE(YSTACK%ZDATA4,1));IF(YLSTACK%L4>YLSTACK%U4)CALL ABOR1_ACC(__FILE__)
 
-#define alloc8(n, s, l) CALL C_F_POINTER(TRANSFER(YLSTACK%L8, CPTR), n, SHAPE=s); n l => n; YLSTACK%L8=YLSTACK%L8+8*SIZE(n);IF(YLSTACK%L8>YLSTACK%U8)CALL ABOR1_ACC(__FILE__)
+#define alloc8(n, s, l) CALL C_F_POINTER(C_LOC(YSTACK%ZDATA8(1,YLSTACK%L8+1)), n, SHAPE=s); n l => n; YLSTACK%L8=YLSTACK%L8+(SIZE(n,KIND=8)/SIZE(YSTACK%ZDATA8,1));IF(YLSTACK%L8>YLSTACK%U8)CALL ABOR1_ACC(__FILE__)
 
 
-#define malign(p,k) ((((p)+(k)-1)/(k)) * (k))
-
-#define stack_l4(ydstack,ibl,nbl) malign(LOC (ydstack%ZDATA4 (1,1,1,1)) + ((INT (ibl, 8) - 1) * SIZE (ydstack%ZDATA4) * KIND (ydstack%ZDATA4)) / INT (nbl, 8), ydstack%IALIGN) 
-#define stack_u4(ydstack,ibl,nbl)        LOC (ydstack%ZDATA4 (1,1,1,1)) + ((INT (ibl, 8)    ) * SIZE (ydstack%ZDATA4) * KIND (ydstack%ZDATA4)) / INT (nbl, 8)
+#define stack_l4(ydstack,ibl,nbl) ((INT (ibl, 8) - 1) * SIZE (ydstack%ZDATA4, 2)) / INT (nbl, 8)
+#define stack_u4(ydstack,ibl,nbl) ((INT (ibl, 8)    ) * SIZE (ydstack%ZDATA4, 2)) / INT (nbl, 8)
   
-#define stack_l8(ydstack,ibl,nbl) malign(LOC (ydstack%ZDATA8 (1,1,1,1)) + ((INT (ibl, 8) - 1) * SIZE (ydstack%ZDATA8) * KIND (ydstack%ZDATA8)) / INT (nbl, 8), ydstack%IALIGN) 
-#define stack_u8(ydstack,ibl,nbl)        LOC (ydstack%ZDATA8 (1,1,1,1)) + ((INT (ibl, 8)    ) * SIZE (ydstack%ZDATA8) * KIND (ydstack%ZDATA8)) / INT (nbl, 8)
-  
-#else
-
-#define temp(t, n, s) t, TARGET, DIMENSION s :: n
-
-#define alloc(n)
-
-#define assoc(p,q) CALL ABOR1_ACC (__FILE__)
-
-#define nullptr(p) CALL ABOR1_ACC (__FILE__)
-
-#define malign(p,k) ((((p)+(k)-1)/(k)) * (k))
-
-#define stack_l4(ydstack,ibl,nbl) 0
-#define stack_u4(ydstack,ibl,nbl) 0
-  
-#define stack_l8(ydstack,ibl,nbl) 0
-#define stack_u8(ydstack,ibl,nbl) 0
-
-#endif
+#define stack_l8(ydstack,ibl,nbl) ((INT (ibl, 8) - 1) * SIZE (ydstack%ZDATA8, 2)) / INT (nbl, 8)
+#define stack_u8(ydstack,ibl,nbl) ((INT (ibl, 8)    ) * SIZE (ydstack%ZDATA8, 2)) / INT (nbl, 8)
   
 #endif
